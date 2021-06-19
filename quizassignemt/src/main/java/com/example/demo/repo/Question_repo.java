8cd5d1f311c6cd004db399a9876f57bfe61cac72
Ghayour Alvi <60334604+GhayourAlvi@users.noreply.github.com>
2021-06-19 22:51:39 +0500

    Refactored

--- quizassignemt/src/main/java/com/example/demo/repo/Question_repo.java
+++ quizassignemt/src/main/java/com/example/demo/repo/Question_repo.java
@@ -3,5 +3,5 @@ package com.example.demo.repo;
 import com.example.demo.model.Question;
 import org.springframework.data.repository.CrudRepository;
 
-public interface question_repo extends CrudRepository<Question,Integer> {
+public interface Question_repo extends CrudRepository<Question,Integer> {
 }
