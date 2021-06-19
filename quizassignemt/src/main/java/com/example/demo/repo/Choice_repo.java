1d009c04309a19eda0aaaa948df887dab5a8c447
Ghayour Alvi <60334604+GhayourAlvi@users.noreply.github.com>
2021-06-19 22:51:04 +0500

    Renamed class

--- quizassignemt/src/main/java/com/example/demo/repo/Choice_repo.java
+++ quizassignemt/src/main/java/com/example/demo/repo/Choice_repo.java
@@ -3,5 +3,5 @@ package com.example.demo.repo;
 import com.example.demo.model.Choice;
 import org.springframework.data.repository.CrudRepository;
 
-public interface choice_repo extends CrudRepository <Choice,Integer> {
+public interface Choice_repo extends CrudRepository <Choice,Integer> {
 }
